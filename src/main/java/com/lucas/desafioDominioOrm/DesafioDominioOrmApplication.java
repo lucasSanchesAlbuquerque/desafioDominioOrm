9831562efba7d7e40b63a2531fdc3d5ccba9ec26
Lucas SA <lusanchesx@gmail.com>
2024-08-27 08:26:26 -0300

    criando projeto e add entidades, participante, bloco, categoria

---
+++ src/main/java/com/lucas/desafioDominioOrm/DesafioDominioOrmApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.desafioDominioOrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioDominioOrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioDominioOrmApplication.class, args);
+	}
+
+}
